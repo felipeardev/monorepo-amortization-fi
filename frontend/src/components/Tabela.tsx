d7ec51e47192fa89ef7aa6c27f6aea2635bd8697
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-19 20:44:12 -0300

    Modificações no componente de tabela

--- frontend/src/components/Tabela.tsx
+++ frontend/src/components/Tabela.tsx
@@ -1,14 +1,60 @@
 import React from "react";
 import { IAmortizacao } from "../interfaces/IAmortizacao";
+import "./styles.css";
 
-interface propsData {
-  data: IAmortizacao
+interface propsAmortizacao {
+  titulo: string;
+  dados: IAmortizacao[];
 }
 
-export function Tabela(props: IAmortizacao[]) {
+export function Tabela(props: propsAmortizacao) {
   return (
-    props.map((item, indice) => {
-      <td key={indice}>item</td>
-    })
-  )
+    <table>
+      <caption>{props.titulo}</caption>
+      <thead>
+        <tr>
+          <th align="center">Periodo</th>
+          <th align="right">Parcela</th>
+          <th align="right">Juros</th>
+          <th align="right">Amortizacao</th>
+          <th align="right">Saldo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.dados.map((item) => {
+          return (
+            <tr>
+              <td key={item.periodo} align="center">
+                {item.periodo}
+              </td>
+              <td key={item.periodo} align="right">
+                {item.parcela.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "brl",
+                })}
+              </td>
+              <td key={item.periodo} align="right">
+                {item.juros.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "brl",
+                })}
+              </td>
+              <td key={item.periodo} align="right">
+                {item.amortizacao.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "brl",
+                })}
+              </td>
+              <td key={item.periodo} align="right">
+                {item.saldo.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "brl",
+                })}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
