8fecc11f685ef51b13f95c17839eb8995c9982eb
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-19 20:44:39 -0300

    Estilização e reutilização do componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,101 +1,73 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Amortizacao, IAmortizacao } from './interfaces/IAmortizacao';
+import React, { useState } from "react";
+import axios from "axios";
+import { IAmortizacao } from "./interfaces/IAmortizacao";
+import { Tabela } from "./components/Tabela";
+import "./App.css";
 
 const initialState = {
-  emprestimo: 0,
-  quantidadeDeParcelas: 0,
-  taxa: 0
+  emprestimo: "",
+  quantidadeDeParcelas: "",
+  taxa: "",
 };
 
 function App() {
-
   const [visible, setVisible] = useState(false);
-  const [dados, setDados] = useState<Amortizacao>(initialState);
+  const [dados, setDados] = useState<any>(initialState);
   const [sac, setSac] = useState<IAmortizacao[]>([]);
   const [price, setPrice] = useState<IAmortizacao[]>([]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const sac = await axios.post<IAmortizacao[]>('http://localhost:3333/sac', dados).then(sac => setSac(sac.data));
-    const price = await axios.post('http://localhost:3333/price', dados).then(price => setPrice(price.data));
+    const sac = await axios
+      .post<IAmortizacao[]>("http://localhost:3333/sac", dados)
+      .then((sac) => setSac(sac.data));
+    const price = await axios
+      .post("http://localhost:3333/price", dados)
+      .then((price) => setPrice(price.data));
     setVisible(true);
-  }
+  };
 
   return (
-    <>
+    <div>
+      {!visible && <h1>Por favor, inserir os dados abaixo.</h1>}
       <form onSubmit={handleSubmit}>
         <label>Emprestimo:</label>
-        <input type="number" name="emprestimo" onChange={(e) => setDados({...dados, emprestimo: Number(e.target.value)})} value={dados.emprestimo} required/><br/>
+        <input
+          type="text"
+          name="emprestimo"
+          onChange={(e) => setDados({ ...dados, emprestimo: e.target.value })}
+          value={dados.emprestimo}
+          required
+        />
+        <br />
 
         <label>Parcelas:</label>
-        <input type="number" name="quantidadeDeParcelas" onChange={(e) => setDados({...dados, quantidadeDeParcelas: Number(e.target.value)})} value={dados.quantidadeDeParcelas} required/><br/>
+        <input
+          type="text"
+          name="quantidadeDeParcelas"
+          onChange={(e) =>
+            setDados({ ...dados, quantidadeDeParcelas: e.target.value })
+          }
+          value={dados.quantidadeDeParcelas}
+          required
+        />
+        <br />
 
         <label>Taxa:</label>
-        <input type="number" name="taxa" onChange={(e) => setDados({...dados, taxa: Number(e.target.value)})} value={dados.taxa} required/><br/>
-
+        <input
+          type="text"
+          name="taxa"
+          onChange={(e) => setDados({ ...dados, taxa: e.target.value })}
+          value={dados.taxa}
+          required
+        />
+        <br />
         <button>Confirmar</button>
       </form>
-      { 
-        visible &&
-        <table>
-          Tabela Price
-          <tr>
-            <th>Periodo</th>
-            <th>Parcela</th>
-            <th>Juros</th>
-            <th>Amortizacao</th>
-            <th>Saldo</th>
-          </tr>
-
-            {
-              price.map((item, indice) => {
-                return (
-                  <>          
-                    <tr>
-                      <td key={indice}>{Number(item.periodo).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.parcela).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.juros).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.amortizacao).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.saldo).toFixed(4)}</td>
-                    </tr>
-                  </>
-                )
-              })
-            }
-        </table>
-      }
-            { 
-        visible &&
-        <table>
-          Tabela Sac
-          <tr>
-            <th>Periodo</th>
-            <th>Parcela</th>
-            <th>Juros</th>
-            <th>Amortizacao</th>
-            <th>Saldo</th>
-          </tr>
-
-            {
-              sac.map((item, indice) => {
-                return (
-                  <>          
-                    <tr>
-                      <td key={indice}>{Number(item.periodo).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.parcela).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.juros).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.amortizacao).toFixed(4)}</td>
-                      <td key={indice}>{Number(item.saldo).toFixed(4)}</td>
-                    </tr>
-                  </>
-                )
-              })
-            }
-        </table>
-      }
-    </>
-  )
+      {visible && <Tabela titulo="Tabela Price" dados={price} />}
+      {visible && <Tabela titulo="Tabela SAC" dados={sac} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
